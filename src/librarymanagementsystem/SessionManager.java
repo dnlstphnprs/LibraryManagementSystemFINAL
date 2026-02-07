80a82d51fd21f3e3cfde7b6d517d60d9459322f3
LOUISSE KEITH <LOUISSE KEITH@Dokja>
2026-02-07 23:28:25 +0800

    initial commit

---
+++ src/librarymanagementsystem/SessionManager.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package librarymanagementsystem;
+
+/**
+ *
+ * @author Daeniel Presa
+ */
+public class SessionManager {
+    public static String userID;
+    public static String userName;
+    public static String role;
+}
